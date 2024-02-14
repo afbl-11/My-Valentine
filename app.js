040458e1c23b2c53feda7c2843f2990958fa84ec
Jurell Afable <133481515+afbl-11@users.noreply.github.com>
2024-02-14 18:30:38 +0800

    Iphone x responsive

--- app.js
+++ app.js
@@ -56,10 +56,3 @@ function createBalloons() {
 }
 createBalloons()
 
-/* Fails to create a balloon div */
-
-
-/* suggestions
-  happy heart button should only be clicked once
-  happy heart should i think make an animation going up
-*/
